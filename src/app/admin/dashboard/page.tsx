01f14cfbe2e0a4224a4b6801b1b8e310a38309d6
Rianca Aril <rianca04@gmail.com>
2024-12-13 17:46:15 +0700

    ini update repository ke dua

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,6 +1,31 @@
-import React from 'react'
+"use client";
+import { useAppDispatch } from "@/redux/hooks";
+import  React, { useEffect, useState } from "react";
+
+export interface Iproduct{
+  _id: string;
+  imgSrc: string;
+  fileKey: string;
+  name: string;
+  price: string;
+  category: string; 
+}
+
 
 const Dashboard = () => {
+  const [products, setProducts] = useState([]);
+  const [openPopup, setOpenPopup] = useState(false);
+  const [updateTable, setUpdateTable] = useState(false);
+
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(setLoading(true))
+
+    axios.get("/api/get_products").then((res) => setProducts(res.data)).catch((err) => console.log(err)).finally(() => disspatch(setLoading(false)));
+
+  },[updateTable])
+
   return (
     <div>Dashboard</div>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,18 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: "#0989FF",
+        topHeadingPrimary: "#010f1c",
+        topHeadingSecondary: "#021d35",
+        pink: "#FD4868",
+      },
+      container: {
+        center: true,
+        padding: "15px",
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
